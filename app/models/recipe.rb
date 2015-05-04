b5ca9258c683cfb4e21530819c66e686290fadf3
Laurie Alaoui <laurie.alaoui@gmail.com>
2015-05-04 22:18:27 +0000

    chef and recipe model tests

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ActiveRecord::Base
-  
+  validates :name, presence: true, length: { minimum: 4, maximum: 100 }
+  validates :summary, presence: true, length: { minimum: 10, maximum: 150 }
+  validates :description, presence: true, length: { minimum: 50, maximum: 800 }
 end
+
