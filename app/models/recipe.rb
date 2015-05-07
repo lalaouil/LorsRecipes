367ed97d2c0f1a0dc883de25c165ab3024dc7026
Laurie Alaoui <laurie.alaoui@gmail.com>
2015-05-07 19:56:47 +0000

    TDD and validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ActiveRecord::Base
+  belongs_to :chef
   validates :name, presence: true, length: { minimum: 4, maximum: 100 }
   validates :summary, presence: true, length: { minimum: 10, maximum: 150 }
   validates :description, presence: true, length: { minimum: 50, maximum: 800 }
