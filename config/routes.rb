77265fa8a5d30a74914ec44705d0d0d37ee61947
Laurie Alaoui <laurie.alaoui@gmail.com>
2015-07-13 05:21:17 +0000

    add authentication, pagination, likes and chef index

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,15 @@ Rails.application.routes.draw do
     member do
       post 'like'
     end
-    
   end
+  
+  resources :chefs, except: [:new]
+  
+  get '/register', to: 'chefs#new'
+  
+  get '/login', to: 'logins#new'
+  post '/login', to: 'logins#create'
+  get '/logout', to: 'logins#destroy'
   
 end
  
