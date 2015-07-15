ee9ba08ff3ac6324937c1724d10ecfa0fb24757e
Laurie Alaoui <laurie.alaoui@gmail.com>
2015-07-15 02:59:34 +0000

    create chefs resources and add authentication

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -3,7 +3,7 @@ class ChefsController < ApplicationController
   before_action :require_same_user, only: [:edit, :update]
   
   def index
-    @chefs = Chef.paginate(page: params[:page], per_page: 3)
+    @chefs = Chef.paginate(page: params[:page], per_page: 4)
   end
     
   def new
