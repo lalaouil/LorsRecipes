52d54a191f6b87eef0d76421b3c7b8a6b3ef5363
Laurie Alaoui <laurie.alaoui@gmail.com>
2015-08-17 23:49:43 +0000

    made changes to recipes controller to fix bug

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,10 +4,10 @@ class RecipesController < ApplicationController
   before_action :require_user_like, only: [:like]
   before_action :require_same_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
-  
+
   def index
     @recipes = Recipe.paginate(page: params[:page], per_page: 4)
-  end 
+  end
   
   def show
     
@@ -35,7 +35,7 @@ class RecipesController < ApplicationController
   def update
     if @recipe.update(recipe_params)
       redirect_to recipe_path(@recipe)
-    else 
+    else
       render :edit
     end
   end
@@ -45,43 +45,43 @@ class RecipesController < ApplicationController
     if like.valid?
       redirect_to :back
     else
-      flash[:danger] ="Tsk tsk. No fair voting more than once!"
+      flash[:danger] = "Tsk, tsk, tsk! No fair voting on a recipe more than once."
       redirect_to :back
     end
   end
   
   def destroy
     Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe Deleted"
     redirect_to recipes_path
   end
   
+  private
   
-    private
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description, :picture, style_ids: [], ingredient_ids: [])
+    end
+    
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+    
+    def require_same_user
+      if current_user != @recipe.chef and !current_user.admin?
+        flash[:danger] = "I'm sorry. You may only edit your own recipes."
+        redirect_to recipes_path
+      end
+    end
+    
+    def require_user_like
+      if !logged_in?
+        flash[:danger] = "You must be logged in to do that."
+        redirect_to :back
+      end
+    end
+    
+    def admin_user
+      redirect_to recipes_path unless current_user.admin?
+    end
   
-      def recipe_params
-        params.require(:recipe).permit(:name, :summary, :description, :picture, diet_ids: [], course_ids: [])
-      end
-      
-      def set_recipe
-        @recipe = Recipe.find(params[:id])
-      end
-      
-      def require_same_user
-        if current_user != @recipe.chef and !current_user.admin?
-          flash[:danger] = "I'm sorry. You may only edit your own recipes."
-          redirect_to recipe_path
-        end
-      end
-      
-      def require_user_like
-        if !logged_in?
-          flash[:danger] = "Uh oh. You must be logged in to perform that action." 
-          redirect_to :back
-        end
-      end
-
-      def admin_user
-        redirect_to recipe_path unless current_user.admin?
-      end
-
 end
